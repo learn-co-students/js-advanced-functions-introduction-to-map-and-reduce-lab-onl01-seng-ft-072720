e056f5693f857395359f7de9c52cab627decca68
DavidFuents <Davidf0827@gmail.com>
2020-11-16 20:12:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(x => x * -1)
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(x => x)
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(x => x * 2)
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(x => x*x)
+}
+
+function reduceToTotal(sourceArray, startingPoint= 0) {
+  let total = startingPoint;
+  for (let i = 0; i < sourceArray.length; i++) {
+    total += sourceArray[i];
+  }
+  return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) {
+        return false
+    }
+  }
+  return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) {
+          return true;
+        }
+    }
+    return false;
+}
